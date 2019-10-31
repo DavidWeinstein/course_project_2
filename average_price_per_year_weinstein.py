69e5d0882154a5ad3c2c4ce5b7321ce8761e7386
David Weinstein <weinstein.dev@gmail.com>
2019-10-31 11:28:43 -0400

    avg price per year working

--- average_price_per_year_weinstein.py
+++ average_price_per_year_weinstein.py
@@ -9,29 +9,26 @@ Pseudocode:
 """
 import math
 
-YEARS = ['1993','1994','1995','1996']
+YEARS = {'1993':0.0, '1994':0.0, '1995':0.0, '1996':0.0, '1997':0.0, '1998':0.0, '1999':0.0, '2000':0.0, '2001':0.0, '2002':0.0, '2003':0.0, '2004':0.0, '2005':0.0, '2006':0.0, '2007':0.0, '2008':0.0, '2009':0.0, '2010':0.0, '2011':0.0, '2012':0.0, '2013':0.0}
+YEARS_COUNT = {'1993':0.0, '1994':0.0, '1995':0.0, '1996':0.0, '1997':0.0, '1998':0.0, '1999':0.0, '2000':0.0, '2001':0.0, '2002':0.0, '2003':0.0, '2004':0.0, '2005':0.0, '2006':0.0, '2007':0.0, '2008':0.0, '2009':0.0, '2010':0.0, '2011':0.0, '2012':0.0, '2013':0.0}
 
 gasFile = open("../GasPrices.txt", 'r')
 lines = gasFile.readlines()
 splitLines = []
 for line in lines:
     splitLines.append(line.split(':'))
-yearlyPrices = []
-date = ''
-price = ''
+
 for ele in splitLines:
     date = ele[0]
+    splitDate = date.split('-')
+    year = splitDate[-1]
     price = float(ele[1])
+    YEARS[year] += price
+    YEARS_COUNT[year] += 1
+
 count = 0
 while count < len(YEARS):
-    if YEARS[count] in date:
-        yearlyPrices.append(price)
-        avg = "%.2f" % (sum(yearlyPrices) / len(yearlyPrices))
-        print(avg)
-    count += 1
-    
-# print(len(YEARS))
-# print(YEARS)
-# print(ninetyThree)
-# print("%.2f" % (sum(ninetyThree) / len(ninetyThree)))
-# print(splitLines)
+    for key in YEARS:
+        avg = YEARS[key] / YEARS_COUNT[key]
+        count +=1
+        print("The average price in", key, "was $", "%.2f" % avg)
