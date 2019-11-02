9479334e2c8bfd1950c98f544a5cfbb1e368ac5b
David Weinstein <weinstein.dev@gmail.com>
2019-11-01 21:58:51 -0400

    got a working avg

--- average_price_per_month_weinstein.py
+++ average_price_per_month_weinstein.py
@@ -6,8 +6,8 @@ File name: average_price_per_month_weinstein.py
 
 Pseudocode:
 """
-
-MONTHS = {'01':'January', '02':'February', '03':'March', '04':'April', '05':'May', '06':'June', '07':'July', '08':'August', '09':'September', '10':'October', '11':'November', '12':'December'}
+MONTHS = ['04', '05', '06', '07', '08', '09', '10', '11', '12', '01', '02', '03']
+MONTHS_TO_TEXT = {'01':'January', '02':'February', '03':'March', '04':'April', '05':'May', '06':'June', '07':'July', '08':'August', '09':'September', '10':'October', '11':'November', '12':'December'}
 YEARS = ['1993', '1994', '1995', '1996', '1997', '1998', '1999', '2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013']
 
 def manageGasFile(f):
@@ -17,6 +17,7 @@ def manageGasFile(f):
 
 def splitLines(lyst):
     newList = []
+    mainList = []
     for line in lyst:
         newList.append(line.split(':'))
     for ele in newList:
@@ -25,15 +26,21 @@ def splitLines(lyst):
         month = splitDate[0]
         year = splitDate[-1]
         price = float(ele[1])
-        newList = [MONTHS[month], year, price]
-        countAvg(newList)
+        mainList.append([month, year, price])
+    countAvg(mainList)
 
 def countAvg(lyst):
     total = 0.0
-    price = float(lyst[-1])
+    count = 0
+    avgList = []
     for ele in lyst:
-        total += price
-        print(total) 
+        if ele[0] == MONTHS[0] and ele[1] == YEARS[0]:
+            avgList.append(ele[2])
+            avg = sum(avgList) / len(avgList)
+    print(avg)
+
+
+            
 
 def main():
     manageGasFile("../GasPrices.txt")
